da7b046344b25a1a3a4a299eade62388bff570e7
olliveira-gabriel <gshooter03@gmail.com>
2025-10-07 22:21:55 -0300

    07/10

--- src/Components/Evento.jsx
+++ src/Components/Evento.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import FormEvento from './FormEvento'
-function Meuevento() {
-    console.log('fui ativado')
-}
+import Button from './Button'
 
 function Evento() {
+  function PrimeiroEvento() {
+      console.log('primeiro evento ativado')
+  }
+  function SegundoEvento(){
+      console.log('Segundo evento ativado')
+
+  }
   return (
     <div> <p>Clique para disparar um evento </p>
-        <button onClick={Meuevento}>Ativar</button>
-        <FormEvento/>
+        <Button event={PrimeiroEvento} text='Primeiro Evento'/>
+        <Button event={SegundoEvento} text='Segundo Evento'/>
+        {/* <FormEvento/> */}
     </div>
   )
 }
